3c79be753a74a79128f25f4750b24717b0c84327
OttohCharlesWills <ottohcharleswills@gmail.com>
2024-08-07 13:44:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+import Courses from './components/courses/course';
+import Body from './components/body/body';
+import Select from './components/select/select';
+import Slideshow from './components/slideshow/slideshow';
+import Footer from './components/footer/footer';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Courses/>
+      <Body/>
+      <Select/>
+      <Slideshow/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+
